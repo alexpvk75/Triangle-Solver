57dfe8d8762ceda95215853d3c104eb58dcfb67a
Alex Pavlyk <sashapvk7@gmail.com>
2025-11-26 11:37:32 +0000

    triangle builder optimized

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Inizializzazione del dizionario per i dati del triangolo
 import math
-triangolo = {
+superTriangolo = {
     'L0': {'E': 0, 'V': None, 'N': None}, #base
-    'L1': {'E': 0, 'V': None, 'N': None}, #primo lato adjacente
-    'L2': {'E': 0, 'V': None, 'N': None}, #secondo lato adjacente
+    'L1': {'E': 0, 'V': None, 'N': None}, #primo lato adiacente
+    'L2': {'E': 0, 'V': None, 'N': None}, #secondo lato adiacente
     'A0': {'E': 0, 'V': None, 'N': None, 'R': None}, #angolo opposto alla base
     'A1': {'E': 0, 'V': None, 'N': None, 'R': None}, #primo angolo adiacente
     'A2': {'E': 0, 'V': None, 'N': None, 'R': None}, #secondo angolo adiacente
@@ -11,25 +11,40 @@ triangolo = {
     'AREA': {'V': None}, #area
     'PERIMETRO': {'V': None} #perimetro
 }
-lati = ["AB", "BC", "AC"]
-constrlati = [None, None, None]
-angoli = ["C", "A", "B"]
+primeTriangolo = {
+    'ordine_lati': {
+        "AB": ["AB", "BC", "AC"],
+        "BC": ["BC", "AC", "AB"],
+        "AC": ["AC", "AB", "BC"]
+    },
+    'ordine_angoli': {
+        "AB": ["C", "A", "B"],
+        "BC": ["A", "B", "C"],
+        "AC": ["B", "C", "A"]
+    }
+}
+subTriangolo = {
+    'L0': {'V': None, 'S': None}, #base
+    'L1': {'V': None, 'S': None}, #primo lato
+    'L2': {'V': None, 'S': None}, #secondo lato
+    'A0': {'V': None, 'S': None, 'R': None}, #angolo opposto alla base
+    'A1': {'V': None, 'S': None, 'R': None}, #primo angolo
+    'A2': {'V': None, 'S': None, 'R': None}, #secondo angolo
+    'H': {'V': None}, #altezza
+}
 # Inizializzazione della base e della althezza
 while True:
-    base_input = input('\033[36m' + "Quale lato viene considerato come la base (AB/BC/AC): " + '\033[0m').strip().upper()
-    if base_input in lati:
-        L_root = lati.index(base_input)
+    radice = input('\033[36m' + "Quale lato viene considerato come la base (AB/BC/AC): " + '\033[0m').strip().upper()
+    if radice in primeTriangolo['ordine_lati']:
         break
     print('\033[91m' + "Input non valido! Inserisci AB, BC o AC" + '\033[0m')
-constrlati[0] = int(2*(L_root%2)+(1 - (L_root%2))*(1 - ((2-L_root)/2)))
-constrlati[1] = int((constrlati[0] % 2) * (constrlati[0] + 1) + (1 - (constrlati[0] % 2)) * (1 - ((constrlati[0])/2)))
-constrlati[2] = int((1 - (constrlati[0] % 2)) * (2 - constrlati[0]/2))
-for lato in lati:
-    triangolo[f'L{constrlati[lati.index(lato)]}']['N'] = lato
-for angolo in angoli:
-    triangolo[f'A{constrlati[angoli.index(angolo)]}']['N'] = angolo
+for indice in range(0, 3):
+    superTriangolo[f'L{indice}']['N'] = primeTriangolo['ordine_lati'][radice][indice]
+for indice in range(0, 3):
+    superTriangolo[f'A{indice}']['N'] = primeTriangolo['ordine_angoli'][radice][indice]
+
 if input('\033[36m' + "Si conosce l'altezza relativa alla base? (S/N): "+ '\033[0m').strip().upper() == "S":
-    triangolo['H']['E'] = 1
+    superTriangolo['H']['E'] = 1
 # Funzioni di conversione
 def convlato(valore):
     if valore.upper() == "X":
@@ -49,13 +64,12 @@ def convangolo(valore):
         return None
 # Input dei dati
 for chiave in range(0,3):
-    triangolo[f'L{chiave}']['V'] = convlato(input(f"Inserisci il valore del lato {triangolo[f'L{chiave}']['N']} (inserisci 'X' se sconosciuto): "))
-    triangolo[f'L{chiave}']['E'] = 1 if triangolo[f'L{chiave}']['V'] != None else 0
+    superTriangolo[f'L{chiave}']['V'] = convlato(input(f"Inserisci il valore del lato {superTriangolo[f'L{chiave}']['N']} (inserisci 'X' se sconosciuto): "))
+    superTriangolo[f'L{chiave}']['E'] = 1 if superTriangolo[f'L{chiave}']['V'] != None else 0
 for chiave in range(0, 3):
-    triangolo[f'A{chiave}']['V'] = convangolo(input(f"Inserisci il valore dell'angolo {triangolo[f'A{chiave}']['N']} (inserisci 'X' se sconosciuto): "))
-    triangolo[f'A{chiave}']['E'] = 1 if triangolo[f'A{chiave}']['V'] != None else 0
-    triangolo[f'A{chiave}']['R'] = math.radians(float(triangolo[f'A{chiave}']['V'])) if triangolo[f'A{chiave}']['V'] != None else None
+    superTriangolo[f'A{chiave}']['V'] = convangolo(input(f"Inserisci il valore dell'angolo {superTriangolo[f'A{chiave}']['N']} (inserisci 'X' se sconosciuto): "))
+    superTriangolo[f'A{chiave}']['E'] = 1 if superTriangolo[f'A{chiave}']['V'] != None else 0
+    superTriangolo[f'A{chiave}']['R'] = math.radians(float(superTriangolo[f'A{chiave}']['V'])) if superTriangolo[f'A{chiave}']['V'] != None else None
 
-if triangolo['H']['E'] == 1:
-    triangolo['H']['V'] = convlato(input(f"Inserisci il valore di altezza H relativa a {triangolo['L0']['N']} (inserisci 'X' se sconosciuto): "))
-print(triangolo)
+if superTriangolo['H']['E'] == 1:
+    superTriangolo['H']['V'] = convlato(input(f"Inserisci il valore di altezza H relativa a {superTriangolo['L0']['N']} (inserisci 'X' se sconosciuto): "))
