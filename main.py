6b32d581f82c71a101fb4b8083cd904c6f62e870
Alex Pavlyk <sashapvk7@gmail.com>
2025-12-09 14:58:13 +0100

    ottimizato

--- main.py
+++ main.py
@@ -1,39 +1,56 @@
-# 0 - valori lati (AB, BC, AC)
-# 1 - valori angoli (C, A, B)
-# 2 - area, perimetro
 import math
-triangolo = [[0, 0, 0],
-             [0, 0, 0],
-             [0, 0]]
-for i in range(0, 3):
+L = [0, 0, 0] # Lati del triangolo
+A = [0, 0, 0] # Angoli del triangolo
+perimetro = 0
+area = 0
+for i in range(3):
     while True:
-        insert = input(f"Inserisci il valore del lato {['AB', "BC", "AC"][i]}: ").strip().upper()
+        insert = input(f"Inserisci il valore del lato {["AB", "BC", "AC"][i]}: ").strip().upper()
         try:
             valore = float(insert)
+            if valore % 1 == 0:
+                valore = int(insert)
         except ValueError:
             print("Inserisci un numero valido")
             continue
-        if float(insert) <= 0:
+        if valore <= 0:
             print("Il valore non può essere minore o uguale a 0")
             continue
-        triangolo[0][i] = float(insert)
+        L[i] = valore
         break
 
 def controllare(L):
     return (L[0] + L[1] > L[2]) and (L[1] + L[2] > L[0]) and (L[0] + L[2] > L[1])
 
-if(not controllare(triangolo[0])):
+if(not controllare(L)):
     print("I valori inseriti non possono formare un triangolo valido.")
 else:
-    L = [triangolo[0][0], triangolo[0][1], triangolo[0][2]]
-    semiper = (L[0] + L[1] + L[2]) / 2
-    area = math.sqrt(semiper * (semiper - L[0]) * (semiper - L[1]) * (semiper - L[2]))
-    triangolo[2][0] = area
-    triangolo[2][1] = semiper * 2
+    perimetro = (L[0] + L[1] + L[2])
+    perimetro = int(perimetro) if perimetro % 1 == 0 else round(float(perimetro), 2)
+    semiper = perimetro / 2
+    erona = math.sqrt(semiper * (semiper - L[0]) * (semiper - L[1]) * (semiper - L[2]))
+    area = int(erona) if erona % 1 == 0 else round(float(erona), 2)
     A = [0, 0, 0]
-    A[0] = math.acos((L[1]**2+L[2]**2-L[0]**2)/(2*L[1]*L[2])) * (180/math.pi)
-    A[1] = math.acos((L[0]**2+L[2]**2-L[1]**2)/(2*L[0]*L[2])) * (180/math.pi)
-    A[2] = 180 - A[0] - A[1]
-    for i in range(0, 3):
-        triangolo[1][i] = A[i]
-    print(f"Area: {triangolo[2][0]}\nPerimetro: {triangolo[2][1]}\nAngoli: C={triangolo[1][0]}, A={triangolo[1][1]}, AB={triangolo[1][2]}")
+    def carnot(x, L):
+        y = int(((x%2)*(x+1)+(1-(x%2))*(1-(x/2))))
+        z = int((1-(x%2))*(2-(x/2)))
+        cos = (L[y]**2 + L[z]**2 - L[x]**2) / (2 * L[y] * L[z])
+        angolo = math.degrees(math.acos(cos))
+        if angolo % 1 == 0:
+            return int(angolo)
+        else:
+            return float(angolo)      
+    for x in range(3):
+        A[x] = carnot(x, L)
+    print(f"\nRISULTATI\nArea: {area}\nPerimetro: {perimetro}\nAngoli: C={A[0]}, A={A[1]}, B={A[2]}")
+    fileTXT = [
+        f"Lati: AB={L[0]}, BC={L[1]}, AC={L[2]}\n",
+        f"C=arccos((AC^2+BC^2-AB^2)/(2*AC*BC))=arccos(({L[1]}^2+{L[2]}^2-{L[0]}^2)/(2*{L[1]}*{L[2]}))={A[0]}\n",
+        f"A=arccos((AB^2+AC^2-BC^2)/(2*AB*AC))=arccos(({L[2]}^2+{L[0]}^2-{L[1]}^2)/(2*{L[2]}*{L[0]}))={A[1]}\n",
+        f"B=arccos((AB^2+BC^2-AC^2)/(2*AB*BC))=arccos(({L[0]}^2+{L[1]}^2-{L[2]}^2)/(2*{L[0]}*{L[1]}))={A[2]}\n\n",
+        f"Perimetro: AB+BC+AC={L[0]}+{L[1]}+{L[2]}={perimetro}\n",
+        f"p=Perimetro/2={perimetro}/2={semiper}\n",
+        f"Area: sqrt(p(p-AB)(p-BC)(p-AC))=sqrt({semiper}*({semiper}-{L[0]})*({semiper}-{L[1]})*({semiper}-{L[2]}))={area}\n"
+    ]
+    with open("soluzione.txt", "w") as file:
+        file.writelines(fileTXT)
--- soluzione.md
+++ soluzione.md
@@ -1,7 +1,7 @@
-Sia $AB, BC, AC$ - lati del triangolo inseriti dall'utente
-
-$$p=\frac{AB + BC + AC}{2}, Perimetro=p\times 2$$
-$$Area=\sqrt{p(p-AB)(p-BC)(p-AC)}$$
-$$C=\arccos{(\frac{AC^2+BC^2-AB^2}{2\times AC\times BC})}$$
-$$A=\arccos{(\frac{AB^2+AC^2-BC^2}{2\times AB\times AC})}$$
-$$B=180-A-C$$
+Lati: $AB=3$, $BC=4$, $AC=5$
+$$C=\arccos{(\frac{AC^2+BC^2-AB^2}{2\times AC\times BC})}=\arccos{(\frac{5^2+4^2-3^2}{2\times 5\times 4})}=36.86989764584401$$
+$$A=\arccos{(\frac{AB^2+AC^2-BC^2}{2\times AB\times AC})}=\arccos{(\frac{3^2+5^2-4^2}{2\times 3\times 5})}=90$$
+$$B=180-C-A=180-90-36.86989764584401=53.13010235415599$$
+$$Perimetro: AB+BC+AC=3+4+5=12$$
+$$p=\frac{Perimetro}{2}=\frac{12}{2}=6.0$$
+$$Area: \sqrt{p(p-AB)(p-BC)(p-AC)}=\sqrt{6.0*(6.0-3)*(6.0-4)*(6.0-5)}=6$$
