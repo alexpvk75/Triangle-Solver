2fa7d32f648df667566d436c32b65232b475460a
Alex Pavlyk <sashapvk7@gmail.com>
2025-12-09 23:14:24 +0100

    test files finalised

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import math
+import os
+os.makedirs("files", exist_ok=True)
 L = [0, 0, 0] # Lati del triangolo (AB, BC, AC)
 A = [0, 0, 0] # Angoli del triangolo (C, A, B)
 perimetro = 0
 area = 0
+# Fase di input
 for i in range(3):
     while True:
         insert = input(f"Inserisci il valore del lato {["AB", "BC", "AC"][i]}: ").strip().upper()
         try:
             valore = float(insert)
-            if valore % 1 == 0:
-                valore = int(insert)
         except ValueError:
             print("Inserisci un numero valido")
             continue
@@ -24,50 +25,53 @@ def valido(L):
 if(not valido(L)):
     print("I valori inseriti non possono formare un triangolo valido.")
 else:
+    # Fase elaaborativa
     perimetro = (L[0] + L[1] + L[2])
-    perimetro = int(perimetro) if perimetro % 1 == 0 else float(perimetro)
     semiper = perimetro / 2
-    semiper = int(semiper) if semiper % 1 == 0 else float(semiper)
     area = math.sqrt(semiper * (semiper - L[0]) * (semiper - L[1]) * (semiper - L[2]))
-    area = int(area) if area % 1 == 0 else float(area)
     A = [0, 0, 0]     
     for x in range(3):
         cos = (L[(x+1)%3]**2 + L[(x+2)%3]**2 - L[x]**2) / (2 * L[(x+1)%3] * L[(x+2)%3])
-        angolo = math.degrees(math.acos(cos))
-        A[x] = int(angolo) if angolo % 1 == 0 else float(angolo)
-    def format(num, cifre=2):
-        if num % 1 == 0:
+        A[x] = math.degrees(math.acos(cos))
+    # Fase di output
+    def fmt(num, cifre=3):
+        if num == int(num):
             return str(num)
         else:
-            return f"{num:.{cifre}f}"
+            return f"{num:.{cifre}f}".rstrip('0').rstrip('.')
     text = {
         '.txt': [
             'Soluzione:\n',
-            f'Lati: AB = {format(L[0])}, BC = {L[1]}, AC = {L[2]}\n',
-            f'Perimetro: AB + BC + AC = {perimetro}\n',
+            f'Lati: AB = {L[0]}, BC = {L[1]}, AC = {L[2]}\n',
+            f'Perimetro: AB + BC + AC = {fmt(perimetro)}\n',
             f'Area: sqrt(p(p-AB)(p-BC)(p-AC))\n',
-            f'p = (AB+BC+AC)/2 = {semiper}\n',
-            f'Area: sqrt({semiper}({semiper}-{L[0]})({semiper}-{L[1]})({semiper}-{L[2]})) = {area}\n',
-            f'C = arccos((BC^2 + AC^2 - AB^2) / (2 * BC * AC)) = {A[0]}\n',
-            f'A = arccos((AB^2 + AC^2 - BC^2) / (2 * AB * AC)) = {A[1]}\n',
-            f'B = arccos((AB^2 + BC^2 - AC^2) / (2 * AB * BC)) = {A[2]}\n'
-        ],
+            f'p = (AB+BC+AC)/2 = {fmt(semiper)}\n',
+            f'Area: sqrt({fmt(semiper)}({fmt(semiper)}-{L[0]})({fmt(semiper)}-{L[1]})({fmt(semiper)}-{L[2]}))'
+            f' = {fmt(area)}\n'],
         '.md': [
             '## Soluzione:\n',
-            f'- $$Lati: AB = {L[0]}, BC = {L[1]}, AC = {L[2]}$$\n',
-            f'- $$Perimetro: AB + BC + AC = {perimetro}$$\n',
-            f'- $$Area: \\sqrt{{p(p-AB)(p-BC)(p-AC)}}$$\n',
-            f'- $$p = \\frac{{AB+BC+AC}}{{2}} = {semiper}$$\n',
-            f'- $$Area: \\sqrt{{{semiper}({semiper}-{L[0]})({semiper}-{L[1]})({semiper}-{L[2]})}} = {area}$$\n',
-            f'- $$C = arccos(\\frac{{BC^2 + AC^2 - AB^2}}{{2 \\times BC \\times AC}}) = {A[0]}$$\n',
-            f'- $$A = arccos\\frac{{AB^2 + AC^2 - BC^2}}{{2 \\times AB \\times AC}} = {A[1]}$$\n',
-            f'- $$B = arccos\\frac{{AB^2 + BC^2 - AC^2}}{{2 \\times AB \\times BC}} = {A[2]}$$\n'
+            f'$$Lati: AB = {L[0]}, BC = {L[1]}, AC = {L[2]}$$\n',
+            f'$$Perimetro: AB + BC + AC = {perimetro}$$\n',
+            f'$$Area: \\sqrt{{p(p-AB)(p-BC)(p-AC)}}$$\n',
+            f'$$p = \\frac{{AB+BC+AC}}{{2}} = {fmt(semiper)}$$\n',
+            f'$$Area: \\sqrt{{{fmt(semiper)}({fmt(semiper)}-{L[0]})({fmt(semiper)}-{L[1]})({fmt(semiper)}-{L[2]})}}'
+            f' = {fmt(area)}$$\n',
         ]
     }
-
-    def scrivere(name, format, content):
-        with open("".join(name + format), "w") as file:
+    for x in range(3):
+        text['.txt'].append(
+            f'{["C", "A", "B"][x]} = arccos(({["AB", "BC", "AC"][(x+1)%3]}^2 + {["AB", "BC", "AC"][(x+2)%3]}^2 - {["AB", "BC", "AC"][x]}^2) '
+            f'/ (2 * {["AB", "BC", "AC"][(x+1)%3]} * {["AB", "BC", "AC"][(x+2)%3]})) '
+            f'= arccos(({L[(x+1)%3]}^2 + {L[(x+2)%3]}^2 - {L[x]}^2) / (2 * {L[(x+1)%3]} * {L[(x+2)%3]})) = {fmt(A[x])}\n'
+        )
+        text['.md'].append(
+            f'$$ {["C", "A", "B"][x]} = arccos\\frac{{{["AB", "BC", "AC"][(x+1)%3]}^2 + {["AB", "BC", "AC"][(x+2)%3]}^2 - {["AB", "BC", "AC"][x]}^2}}'
+            f'{{2 \\times {["AB", "BC", "AC"][(x+1)%3]} \\times {["AB", "BC", "AC"][(x+2)%3]}}} '
+            f'= arccos\\frac{{{L[(x+1)%3]}^2 + {L[(x+2)%3]}^2 - {L[x]}^2}}{{2 \\times {L[(x+1)%3]} \\times {L[(x+2)%3]}}} = {fmt(A[x])} $$\n'
+        )
+    def scrivere(name, format, content, dir):
+        with open(os.path.join(dir, "".join(name + format)), "w") as file:
             file.writelines(content[format])
-    scrivere("soluzione", ".txt", text)
-    scrivere("soluzione", ".md", text)
+    scrivere("soluzione", ".txt", text, "files")
+    scrivere("soluzione", ".md", text, "files")
     print(f'\nPerimetro: {perimetro}\nArea: {area}\n')
