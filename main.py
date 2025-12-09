94906c1a22d8929c311d69da8bb303c376480482
Alex Pavlyk <sashapvk7@gmail.com>
2025-12-09 21:31:54 +0100

    text files feature

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ else:
     perimetro = (L[0] + L[1] + L[2])
     perimetro = int(perimetro) if perimetro % 1 == 0 else float(perimetro)
     semiper = perimetro / 2
+    semiper = int(semiper) if semiper % 1 == 0 else float(semiper)
     area = math.sqrt(semiper * (semiper - L[0]) * (semiper - L[1]) * (semiper - L[2]))
     area = int(area) if area % 1 == 0 else float(area)
     A = [0, 0, 0]     
@@ -34,4 +35,39 @@ else:
         cos = (L[(x+1)%3]**2 + L[(x+2)%3]**2 - L[x]**2) / (2 * L[(x+1)%3] * L[(x+2)%3])
         angolo = math.degrees(math.acos(cos))
         A[x] = int(angolo) if angolo % 1 == 0 else float(angolo)
-    print(f"\nRISULTATI\nArea: {area}\nPerimetro: {perimetro}\nAngoli: C={A[0]}, A={A[1]}, B={A[2]}")
+    def format(num, cifre=2):
+        if num % 1 == 0:
+            return str(num)
+        else:
+            return f"{num:.{cifre}f}"
+    text = {
+        '.txt': [
+            'Soluzione:\n',
+            f'Lati: AB = {format(L[0])}, BC = {L[1]}, AC = {L[2]}\n',
+            f'Perimetro: AB + BC + AC = {perimetro}\n',
+            f'Area: sqrt(p(p-AB)(p-BC)(p-AC))\n',
+            f'p = (AB+BC+AC)/2 = {semiper}\n',
+            f'Area: sqrt({semiper}({semiper}-{L[0]})({semiper}-{L[1]})({semiper}-{L[2]})) = {area}\n',
+            f'C = arccos((BC^2 + AC^2 - AB^2) / (2 * BC * AC)) = {A[0]}\n',
+            f'A = arccos((AB^2 + AC^2 - BC^2) / (2 * AB * AC)) = {A[1]}\n',
+            f'B = arccos((AB^2 + BC^2 - AC^2) / (2 * AB * BC)) = {A[2]}\n'
+        ],
+        '.md': [
+            '## Soluzione:\n',
+            f'- $$Lati: AB = {L[0]}, BC = {L[1]}, AC = {L[2]}$$\n',
+            f'- $$Perimetro: AB + BC + AC = {perimetro}$$\n',
+            f'- $$Area: \\sqrt{{p(p-AB)(p-BC)(p-AC)}}$$\n',
+            f'- $$p = \\frac{{AB+BC+AC}}{{2}} = {semiper}$$\n',
+            f'- $$Area: \\sqrt{{{semiper}({semiper}-{L[0]})({semiper}-{L[1]})({semiper}-{L[2]})}} = {area}$$\n',
+            f'- $$C = arccos(\\frac{{BC^2 + AC^2 - AB^2}}{{2 \\times BC \\times AC}}) = {A[0]}$$\n',
+            f'- $$A = arccos\\frac{{AB^2 + AC^2 - BC^2}}{{2 \\times AB \\times AC}} = {A[1]}$$\n',
+            f'- $$B = arccos\\frac{{AB^2 + BC^2 - AC^2}}{{2 \\times AB \\times BC}} = {A[2]}$$\n'
+        ]
+    }
+
+    def scrivere(name, format, content):
+        with open("".join(name + format), "w") as file:
+            file.writelines(content[format])
+    scrivere("soluzione", ".txt", text)
+    scrivere("soluzione", ".md", text)
+    print(f'\nPerimetro: {perimetro}\nArea: {area}\n')
---
+++ soluzione.md
@@ -0,0 +1,9 @@
+## Soluzione:
+- $$Lati: AB = 3, BC = 4, AC = 5$$
+- $$Perimetro: AB + BC + AC = 12$$
+- $$Area: \sqrt{p(p-AB)(p-BC)(p-AC)}$$
+- $$p = \frac{AB+BC+AC}{2} = 6$$
+- $$Area: \sqrt{6(6-3)(6-4)(6-5)} = 6$$
+- $$C = arccos(\frac{BC^2 + AC^2 - AB^2}{2 \times BC \times AC}) = 36.86989764584401$$
+- $$A = arccos\frac{AB^2 + AC^2 - BC^2}{2 \times AB \times AC} = 53.13010235415599$$
+- $$B = arccos\frac{AB^2 + BC^2 - AC^2}{2 \times AB \times BC} = 90$$
